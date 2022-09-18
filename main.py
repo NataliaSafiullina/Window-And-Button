98ff4eb2c144cb6e03efd7fcbf8a5a6f30603b80
NataliaSafiullina <nfsafiullina@gmail.com>
2022-09-18 18:45:02 +0700

    The second commit

--- main.py
+++ main.py
@@ -15,12 +15,17 @@ def clicked_btn2():
 while True:
     window = Tk()
     window.title('Окна и кнопки')
+    # window size
     window.geometry('300x100')
+    # message
     lbl = Label(window, text='Вы долго смотрели в монитор, \nтеперь посмотрите в окно', font=('Arial Bold', 15),
                 justify='center')
     lbl.grid(column=0, row=0, columnspan=2)
+    # the first button
     btn1 = Button(window, text='Закрыть навсегда', command=clicked_btn1)
     btn1.grid(column=0, row=2)
+    # the second button
     btn2 = Button(window, text='Исчезнуть на 10 сек', command=clicked_btn2)
     btn2.grid(column=1, row=2)
+    # loop for window
     window.mainloop()
