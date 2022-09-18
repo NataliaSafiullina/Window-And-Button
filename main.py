983f82089c2dae7dec6a9947b3dbc116407a2976
NataliaSafiullina <nfsafiullina@gmail.com>
2022-09-18 18:34:03 +0700

    The second commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+import time
+
+# close the window and exit program
+def clicked_btn1():
+    #lbl.configure(text='Кнопка была нажата',font=("Arial Bold", 30))
+    quit()
+# hide the window for 10 seconds
+def clicked_btn2():
+    window.withdraw()
+    time.sleep(10)
+    window.deiconify()
+
+
+while True:
+    window = Tk()
+    window.title('Окна и кнопки')
+    window.geometry('300x100')
+    lbl = Label(window, text='Вы долго смотрели в монитор, \nтеперь посмотрите в окно', font=('Arial Bold', 15),
+                justify='center')
+    lbl.grid(column=0, row=0, columnspan=2)
+    btn1 = Button(window, text='Закрыть навсегда', command=clicked_btn1)
+    btn1.grid(column=0, row=2)
+    btn2 = Button(window, text='Исчезнуть на 10 сек', command=clicked_btn2)
+    btn2.grid(column=1, row=2)
+    window.mainloop()
